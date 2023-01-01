c6dd8205f0030d4bc2cf1b52db2e5cdb2788f105
Feynman <lifeng@yanfa.com>
2023-01-01 15:10:00 +0800

    feat: add ext bat board support

--- drivers/gpio/gpio-xbrother.c
+++ drivers/gpio/gpio-xbrother.c
@@ -48,13 +48,23 @@ static ssize_t analog_show(struct kobject *kobj, struct kobj_attribute *attr,
 	{
 		if (attr==&io_attribute[i]){
 
-			adc128s022_xfer(2);
-			gpio_val=gpio_get_value_cansleep(attr_gpios[i-8]);
-			if(gpio_val==0){ //current mode
-				val=ADC128BUF[2*8+i-gpio_num]*10000/1758;
-			}else{ //voltage mode
-				val=ADC128BUF[2*8+i-gpio_num]*10/7;
+			if (i-gpio_num<8) {
+				adc128s022_xfer(2);
+				gpio_val=gpio_get_value_cansleep(attr_gpios[i-8]);
+				if(gpio_val==0){ //current mode
+					val=ADC128BUF[2*8+i-gpio_num]*10000/1758;
+				}else{ //voltage mode
+					val=ADC128BUF[2*8+i-gpio_num]*10/7;
+				}
+			}else{
+				adc128s022_xfer(0);
+				if(i-gpio_num-8==0){
+					val=ADC128BUF[0]*1000/63;
+				}else{
+					val=ADC128BUF[1]*100000/3276;
+				}
 			}
+		
 
 			return sprintf(buf, "%d.%d%d%d\n", val/1000, (val%1000)/100, (val%100)/10, val%10);
 		}
